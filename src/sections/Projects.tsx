4d1062966af736d8804699435b7f32f24a4c1af7
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2026-05-03 14:30:43 +0100

    add github link to view all projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -123,10 +123,12 @@ export const Projects = () => {
 
         {/* View All CTA */}
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
-            View All Projects
-            <ArrowUpRight className="w-5 h-5" />
-          </AnimatedBorderButton>
+          <a href="https://github.com/Perception12" target="_blank">
+            <AnimatedBorderButton>
+              View All Projects
+              <ArrowUpRight className="w-5 h-5" />
+            </AnimatedBorderButton>
+          </a>
         </div>
       </div>
     </section>
